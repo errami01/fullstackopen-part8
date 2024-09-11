346cf78d7f8220aa16cdbbda0f4e5dafe17f34fd
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-09-12 00:34:26 +0100

    Add 8.11: Authors birth year

--- frontend/src/components/Authors.jsx
+++ frontend/src/components/Authors.jsx
@@ -1,8 +1,14 @@
-import { useQuery } from "@apollo/client";
-import { ALL_AUTHORS } from "../queries";
+import { useMutation, useQuery } from "@apollo/client";
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries";
+import { useState } from "react";
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS);
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
   if (!props.show) {
     return null;
   }
@@ -10,7 +16,13 @@ const Authors = (props) => {
     return <div>loading...</div>;
   }
   const authors = [...result.data.allAuthors];
-
+  const updateBirthyear = (event) => {
+    event.preventDefault();
+    editAuthor({ variables: { name, born: parseInt(born) } });
+    console.log(`${name} , ${born}`);
+    setName("");
+    setBorn("");
+  };
   return (
     <div>
       <h2>authors</h2>
@@ -30,6 +42,26 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <div>
+        <h3> Set birthyear</h3>
+        <form onSubmit={updateBirthyear}>
+          <div>
+            name
+            <input
+              value={name}
+              onChange={({ target }) => setName(target.value)}
+            />
+          </div>
+          <div>
+            born
+            <input
+              value={born}
+              onChange={({ target }) => setBorn(target.value)}
+            />
+          </div>
+          <button type="submit">update author</button>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/queries.js
+++ frontend/src/queries.js
@@ -35,3 +35,11 @@ export const ADD_BOOK = gql`
     }
   }
 `;
+export const EDIT_AUTHOR = gql`
+  mutation editAuthor($name: String!, $born: Int!) {
+    editAuthor(name: $name, setBornTo: $born) {
+      name
+      born
+    }
+  }
+`;
