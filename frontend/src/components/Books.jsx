d685e7ba834e428e2f8132e89bc1c8f32eea6184
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-09-11 23:18:32 +0100

    Add 8.10: Adding a book

--- frontend/src/components/Books.jsx
+++ frontend/src/components/Books.jsx
@@ -1,15 +1,7 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { ALL_BOOKS } from "../queries";
 
 const Books = (props) => {
-  const ALL_BOOKS = gql`
-    query {
-      allBooks {
-        title
-        author
-        published
-      }
-    }
-  `;
   const result = useQuery(ALL_BOOKS);
   if (!props.show) {
     return null;
